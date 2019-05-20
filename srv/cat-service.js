fb39942896ff95baff14562628b8e4dd485c74b3
Dr. Christian Lechner <clechner@4process.local>
2019-05-20 15:25:32 +0200

    Small change in CAT service

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -2,7 +2,7 @@ module.exports = srv => {
 
     if (srv.name === 'CatalogService') {
         srv.after('READ', 'Books', each => {
-            if (each.stock > 500) each.title = '(5% off!) ' + each.title
+            if (each.stock > 500) each.title = each.title + ' (5% off!)' 
         })
     }
 
